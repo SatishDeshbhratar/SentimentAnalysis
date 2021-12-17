0d4003b2737ee8e52c4efd09d672f4690ca295e3
SatishDeshbhratar <nbasatish@gmail.com>
2021-12-17 03:00:27 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,45 +1,30 @@
-import numpy as np
-from flask import Flask, request, jsonify, render_template
+from flask import Flask,render_template,url_for,request
+import pandas as pd 
 import pickle
-import traceback
-import pandas as pd
-# Libraries for feature engineering
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.externals import joblib
+import pickle
 
+# load the model from disk
+filename = 'model.pkl'
+clf = pickle.load(open(filename, 'rb'))
+cv=pickle.load(open('tranform.pkl','rb'))
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+	return render_template('home.html')
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
-    try:
-        comment = [str(request.form['inputText'])]
-        print(comment)
-        reshaped_comment = [comment]
-        print(reshaped_comment)
 
-        # build BOW features on train reviews
-        cv = CountVectorizer(binary=False, min_df=0.0, max_df=1.0, ngram_range=(1,2))
-        cv_train_features = cv.fit_transform(reshaped_comment)
+	if request.method == 'POST':
+		message = request.form['message']
+		data = [message]
+		vect = cv.transform(data).toarray()
+		my_prediction = clf.predict(vect)
+	return render_template('result.html',prediction = my_prediction)
 
-        # build TFIDF features on train reviews
-        tv = TfidfVectorizer(use_idf=True, min_df=0.0, max_df=1.0, ngram_range=(1,2),sublinear_tf=True)
-        tv_train_features = tv.fit_transform(reshaped_comment)
-
-        prediction = model.predict(reshaped_comment)
-
-    except Exception:
-        traceback.print_exc()
-
-    return render_template('index.html', prediction_text='Sentiment analysis is - {}'.format(prediction))
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
-    app.config["DEBUG"] = True
+if __name__ == '__main__':
+	app.run(debug=True)
